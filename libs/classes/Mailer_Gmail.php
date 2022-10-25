8c79b5a1fffeca5e1e6028ede38a04d36e78653e
Shah Hussain <shahhussain305@gmail.com>
2022-10-25 19:03:21 +0500

    Removed un-necessary functions
    Removed all the un-necessary functions from the Mailer.php class to make the things simplyfied

--- libs/classes/Mailer_Gmail.php
+++ libs/classes/Mailer_Gmail.php
@@ -40,7 +40,7 @@ class Communicator{
 	/* to send emails from your registered domain / hosting email server
 	private $host = "smtp.hostinger.com";
 	private $userName="no-reply@example.com";
-    private $robots = array(
+    	private $robots = array(
 							'no-reply@example.com',
 							'no-reply1@example.com',
 							'no-reply2@example.com',
@@ -61,112 +61,18 @@ class Communicator{
 	private $host = "smtp.gmail.com";
 	private $userName="abcd1@gmail.com";
 	private $password="ywhstefdirkdfhen";
-    private $robots = array(
-							array('abcd1@gmail.com','ywhstefdirkdfhen'),
-							array('abcd2@gmail.com','oejrgdbeygswjhdo'),
-							array('abcd3@gmail.com','poswkfhrusnfhwlv'),
-							array('abcd4@gmail.com','tgfdeswavfgdertk'),
-							array('abcd5@gmail.com','polkhytrdwesaqzx'),
-							array('abcd6@gmail.com','okljuhbgtrdfcxse')
-							);
+    	private $robots = array(
+				array('abcd1@gmail.com','ywhstefdirkdfhen')
+				);
 
 	public $from_email='abcd1@gmail.com';
 	//--------------------------------------------------------------//
 	public $replyTo="abcd1@gmail.com";
 	var $tempVar;
-    public $from_name = 'NAME OF YOUR ORGANIZATION';
-    public $charSet = "CharSet = 'UTF-8'";
-    public $charSetOpt = 0;
-	//function to send email simple and with attachements
-	public function sendEmail($to,$from,$sender_name="",$subject,$body,$attachement_path="",$cc="",$bcc=""){ 
-			require '../../../../vendor/autoload.php';		
-			$mail = new PHPMailer;
-			$mail->IsSMTP();            // set mailer to use SMTP i.e. smtp1.example.com;smtp2.example.com
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-			$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-			$mail->Host = $this->host;  // specify main and backup server
-			$mail->SMTPAuth = true;     // turn on SMTP authentication
-			$mail->Username = $this->userName;  // SMTP username i.e email id of an email address
-			$mail->Password = $this->password; // SMTP password for the specified email address			
-			$mail->Port = 587;
-			$mail->SMTPSecure = 'tls';
-                        if($this->charSetOpt != 0){
-                           $mail->CharSet = $this->charSet;
-                        }
-			$mail->From = $from;
-			$mail->FromName = $sender_name;
-			$mail->addAddress($to);   //mail,name
-			if(isset($cc) && !empty($cc)){
-				$mail->addBCC($bcc);
-				}
-			if(isset($cc) && !empty($cc)){
-				$mail->addCC($cc);
-				}
-			$mail->addReplyTo($this->replyTo);//to, name			
-			$mail->WordWrap = 50; 
-			if(isset($attachement_path) && !empty($attachement_path)){                                // set word wrap to 50 characters
-					$mail->AddAttachment($attachement_path);         // add attachments
-					}
-			$mail->isHTML(true);                                  // set email format to HTML			
-			$mail->Subject = $subject;
-			$mail->Body = $body;
-			//$mail->AltBody = "This is the body in plain text for non-HTML mail clients";			
-			if(!$mail->send()){
-				$this->tempVar = $mail->ErrorInfo;
-			   return false;
-				}
-			else{
-				return true;
-				}
-			}
-        //for multiple recipients                
-        public function sendEmails($to=array(),$from,$sender_name="",$subject,$body,$attachement_path="",$cc="",$bcc=""){
-			require '../../../../vendor/autoload.php';				
-			$mail = new PHPMailer;
-			$mail->IsSMTP();            // set mailer to use SMTP i.e. smtp1.example.com;smtp2.example.com
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-			$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-			$mail->Host = $this->host;  // specify main and backup server
-			$mail->SMTPAuth = true;     // turn on SMTP authentication
-			$mail->Username = $this->userName;  // SMTP username i.e email id of an email address
-			$mail->Password = $this->password; // SMTP password for the specified email address
-			$mail->Port = 587;
-			$mail->SMTPSecure = 'tls';
-                        if($this->charSetOpt != 0){
-                           $mail->CharSet = $this->charSet;
-                        }
-			$mail->From = $from;
-			$mail->FromName = $sender_name;
-			//$mail->addAddress($to[0]);   //mail,name
-			foreach($to as $value){
-				$mail->addAddress($value); 
-				}
-			if(isset($bcc) && !empty($bcc)){
-				$mail->addBCC($bcc);
-				}
-			if(isset($cc) && !empty($cc)){
-					$mail->addCC($cc);
-				}
-			//$mail->AddAddress("ellen@example.com");                  // name is optional
-			$mail->addReplyTo($this->replyTo);//to, name			
-			$mail->WordWrap = 50; 
-			if(isset($attachement_path) && !empty($attachement_path)){                                // set word wrap to 50 characters
-					$mail->AddAttachment($attachement_path);         // add attachments
-					//$mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
-					}
-			$mail->isHTML(true);                                  // set email format to HTML			
-			$mail->Subject = $subject;
-			$mail->Body = $body;
-			//$mail->AltBody = "This is the body in plain text for non-HTML mail clients";			
-			if(!$mail->send()){
-				$this->tempVar = $mail->ErrorInfo;
-			    return false;
-				}
-			else{
-				return true;
-				}
-			}
-
+    	public $from_name = 'NAME OF YOUR ORGANIZATION';
+    	public $charSet = "CharSet = 'UTF-8'";
+    	public $charSetOpt = 0;
+	
            //for multiple/single recipient(s) and from random robots
            public function sendEmailRnd($to=array(),$sender_name="",$subject,$body,$attachement_path="",$cc="",$bcc=""){ 
 			$this->get_email_user($this->robots);
